99937a1b16634b2489eda8dab796f087d6d8dcd2
serhiy hvala <serhiyhvaladew@gmail.com>
2022-09-04 17:02:02 +0300

    UPD: add accessories section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,28 @@
 import React from "react";
 import Section from "./Section";
-import cartInfo from '../data/carInfo.json'
-import Container from "../styles/HomeStyles"
+import cartInfo from "../data/carInfo.json";
+import Container from "../styles/HomeStyles";
 function Home() {
   return (
     <Container>
-    {cartInfo.map((e) => (
+      {cartInfo.map((e) => (
+        <Section
+          key={e.id}
+          title={e.title}
+          desciption={e.description}
+          backgroundImg={e.image}
+          leftBtnText="Custom Order"
+          rightBtnText="Existing Inventory"
+        />
+      ))}
       <Section
-        key={e.id}
-        title={e.title}
-        desciption={e.description}
-        backgroundImg={e.image}
-        leftBtnText="Custom Order"
-        rightBtnText="Existing Inventory"
+        title="Accessories "
+        desciption=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
       />
-    ))}
     </Container>
   );
 }
 
 export default Home;
-
-
