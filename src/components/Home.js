d49d723a82de9d38268403d2d1a5be5e5a2620b6
serhiy hvala <serhiyhvaladew@gmail.com>
2022-09-04 12:55:51 +0300

    ADD: section for a Tesla

---
+++ src/components/Home.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+function Home() {
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        desciption="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        desciption="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model 3"
+        desciption="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model X"
+        desciption="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Lowest Cost Solar Panels in America"
+        desciption="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Solar for New Roofs"
+        desciption="Produce Clean Energy From Your Roof"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Order now"
+        rightBtnText="Learn more"
+      />
+      <Section
+        title="Accessories "
+        desciption=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
